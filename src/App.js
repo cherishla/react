36822622dc2d9ed3d858d0c6ffcdc7fef09990c4
cherishla <cherishla.fang@gmail.com>
2018-05-31 15:34:15 +0800

    Creating Person Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    //return (
-      // <div className="App">
-      //     <h1>Welcome to React</h1>
-      // </div>
-    //);
-      return React.createElement('div',{className:'App'}, React.createElement('h1', null, 'Welcome to React'));
+    return (
+      <div className="App">
+          <h1>Welcome to React</h1>
+          <p>This is really working!</p>
+          <Person />
+      </div>
+    );
+      // return React.createElement('div',{className:'App'}, React.createElement('h1', null, 'Welcome to React'));
     
   }
 }
