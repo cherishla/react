871ce9520c2f3e6443e4b476c3ce47b012123171
cherishla <cherishla.fang@gmail.com>
2018-06-02 21:25:05 +0800

    change person click event

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@
  const person = (props)=>{
      return (
          <div className="Person">
-         <p onClick={props.click}> I'm a {props.name} and I am {props.age} years old!</p>
+         <p  onClick={props.click}> I'm a {props.name} and I am {props.age} years old!</p>
          <p>{props.children}</p>
          <input type="text" onChange={props.changed} defaultValue={props.name} />
          </div>
